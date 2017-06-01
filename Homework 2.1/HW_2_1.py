8d5cca90ebc6815bebea366c85d39a8eac8c7cf7
Bobby Djunk <bobbydjunk@list.ru>
2017-06-01 22:20:44 +0300

    Rework

--- Homework 2.1/HW_2_1.py
+++ Homework 2.1/HW_2_1.py
@@ -1,39 +1,29 @@
-def reading_file():
-  cook_book = []
-  cb_dict = {}
-  x = int(1)
-  
-  with open('cook_book.txt') as f:
-    for line in f:
-      cook_book += [line.strip()]
-
-  while x < len(cook_book):
-    cb_dict[cook_book[x-1]] = [{'ingridient_name':'', 'quantity': 0, 'measure':''} for i in range(int(cook_book[x]))]
-    for i in range(int(cook_book[x])):
-      cb_dict[cook_book[x-1]][i]['ingridient_name'] += cook_book[i+(x+1)].split(' ')[0]
-      cb_dict[cook_book[x-1]][i]['quantity'] += int(cook_book[i+(x+1)].split(' ')[1])
-      cb_dict[cook_book[x-1]][i]['measure'] += cook_book[i+(x+1)].split(' ')[2]
-    x += int(cook_book[x])+2
-    
-  return cb_dict
+def create_cook_book():
+  with open('cook_book.txt') as file:
+    cb_dict = {}
+    for line in file:
+      cook_book = line.strip()
+      cb_dict[cook_book] = []
+      ingridients = int(file.readline().strip())
+      for i in range(ingridients):
+        product_name, product_quantity, product_measure = (file.readline().strip()).split(' | ')
+        product_quantity = int(product_quantity)
+        cb_dict[cook_book].append({'ingridient_name' : product_name, 'quantity' : product_quantity, 'measure' : product_measure})
+    return cb_dict
 
 def get_shop_list_by_dishes(dishes, person_count):
   shop_list = {}
-  
   for dish in dishes:
-    for ingridient in reading_file()[dish]:
+    for ingridient in create_cook_book()[dish]:
       new_shop_list_item = dict(ingridient)
       new_shop_list_item['quantity'] *= person_count
       if new_shop_list_item['ingridient_name'] not in shop_list:
         shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
       else:
         shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
-        
   return shop_list
 
-def print_shop_list(shop_list): 
-  # for shop_list_item in shop_list.values():
-  #   print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
+def print_shop_list(shop_list):
   for shop_list_item in shop_list.values():
     print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
   
